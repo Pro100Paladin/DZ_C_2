ad1de65983e9fad113a778c4ccb822a8bbcf9ecb
Pro100Paladin <pro100paladin@icloud.com>
2023-07-24 21:35:23 +0300

    задача 13

--- Program.cs
+++ Program.cs
@@ -1,5 +1,16 @@
-﻿// Домашная работа к семенару 2
-// ветки
-// first
-// second
-// third
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+Console.Write("Введите трехзначное число");
+int quad = int.Parse(Console.ReadLine()!);
+string quadString = quad.ToString();
+if (quadString.Length == 3)
+{
+    Console.WriteLine(quadString[2]);
+}
+else
+{
+    Console.WriteLine($"В числе {quadString} третьей цифры нет");
+}
