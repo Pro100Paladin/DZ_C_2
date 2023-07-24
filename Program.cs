c1a68eda29c988a5b579248eb802c6936ebcbbd3
Pro100Paladin <pro100paladin@icloud.com>
2023-07-24 21:27:38 +0300

    задача 10

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
-﻿// Домашная работа к семенару 2
-// ветки
-// first
-// second
-// third
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+
+Console.Write("Введите трехзначное число");
+int trio = int.Parse(Console.ReadLine()!);
+string trioString = trio.ToString();
+if (trioString.Length == 3)
+{
+    Console.WriteLine(trioString[1]);
+}
+else Console.WriteLine("число не корректно");
